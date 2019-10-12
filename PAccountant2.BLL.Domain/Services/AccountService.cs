dc0b386b156e429bd6db8b71695397378344935c
vlad <wbayrakvlad@gmail.com>
2019-10-12 18:39:53 +0300

    PA5/feature/develop - create account infrastructure

---
+++ PAccountant2.BLL.Domain/Services/AccountService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PAccountant2.BLL.Interfaces.Account;
+
+namespace PAccountant2.BLL.Domain.Services
+{
+    public class AccountService : IAccountService
+    {
+    }
+}
---
+++ PAccountant2.BLL.Interfaces/Account/IAccountService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PAccountant2.BLL.Interfaces.Account
+{
+    public interface IAccountService
+    {
+    }
+}
