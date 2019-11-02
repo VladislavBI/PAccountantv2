40035c8cebd0149f5adf5f6cd029e2cfc04eee3d
vlad <wbayrakvlad@gmail.com>
2019-11-02 21:37:33 +0200

    PA21/feature/develop - add tests

---
+++ PAccountant2.BLL.Interfaces/DTO/ViewItems/Account/AccountFilterViewItem.cs
@@ -0,0 +1,9 @@
+﻿namespace PAccountant2.BLL.Interfaces.DTO.ViewItems.Account
+{
+    public class AccountFilterViewItem
+    {
+        public decimal? AmountBellow { get; set; }
+
+        public decimal? AmountAbove { get; set; }
+    }
+}
