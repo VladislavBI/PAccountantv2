1e38e00fe058f27cf127945efc4efaa0f0d8fdb9
vlad <wbayrakvlad@gmail.com>
2019-11-02 22:07:20 +0200

    PA23/feature/develop - investment entity infrastructure created

---
+++ PAccountant2.BLL.Domain/Entities/Investment/InvestmentOperationValueObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using PAccountant2.BLL.Domain.Enum;
+
+namespace PAccountant2.BLL.Domain.Entities.Investment
+{
+    public class InvestmentOperationValueObject
+    {
+        public int Id { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public AccountBalanceChangeType OperationType { get; set; }
+    }
+}
---
+++ PAccountant2.BLL.Domain/Enum/PaymentPeriod.cs
@@ -0,0 +1,10 @@
+﻿namespace PAccountant2.BLL.Domain.Enum
+{
+    public enum PaymentPeriod
+    {
+        Day = 1,
+        Month = 2,
+        Year = 3,
+        Other = 4
+    }
+}
