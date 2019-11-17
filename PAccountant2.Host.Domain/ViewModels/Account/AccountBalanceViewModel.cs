2de6356062838a08ef440788cbf60f8c0b25f8e5
vlad <wbayrakvlad@gmail.com>
2019-11-17 13:46:10 +0200

    PA32/feature/develop - update swagger for account

--- PAccountant2.Host.Domain/ViewModels/Account/AccountBalanceViewModel.cs
+++ PAccountant2.Host.Domain/ViewModels/Account/AccountBalanceViewModel.cs
@@ -1,13 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace PAccountant2.Host.Domain.ViewModels.Account
 {
     public class AccountBalanceViewModel
     {
+        [Required]
         public int Id { get; set; }
 
+        [Required]
+        [Range(0, double.MaxValue, ErrorMessage = "Money amount cann't be negative")]
         public decimal Amount { get; set; }
     }
 }
